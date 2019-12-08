db46f8ff5a6fb08eb7735f4a8544ed45cca657e2
sorhanp <hannupekka.sormunen@gmail.com>
2019-12-08 13:55:08 +0200

    New testing CMakeLists and main_test.c

--- tests/main_tests.c
+++ tests/main_tests.c
@@ -1,4 +1,13 @@
-int main() {
+#include <check.h>
 
-   return 0;
+START_TEST (test_name)
+{
+        int z = 0;
+        ck_assert_int_eq(0, z);
 }
+END_TEST
+
+ int main(void)
+ {
+     return 0;
+ }
